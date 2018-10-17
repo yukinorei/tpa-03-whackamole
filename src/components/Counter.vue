ff748fcac2bf6709fb1ddb8af5094267249e1fa2
yukinorei <>
2018-10-17 14:47:10 +0900

    add Components

---
+++ src/components/Counter.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="counter">
+    <h2>Some Label</h2>
+    <h1>0</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Counter',
+};
+</script>
+
+<style>
+.counter {
+  border: 1px solid #000;
+  margin-top: 20px;
+  padding: 20px;
+}
+
+.counter h1,
+.counter h2 {
+  margin: 0;
+}
+</style>
